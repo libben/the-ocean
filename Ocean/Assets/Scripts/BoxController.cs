c71f5bc8c8be40440c5a22f4c15ea2bb03246487
Vincent Le <vvvle@ucdavis.edu>
2019-11-21 20:53:50 -0800

    Serialized killing velocity and slightly enlarged killing collider for boxes

--- Ocean/Assets/Scripts/BoxController.cs
+++ Ocean/Assets/Scripts/BoxController.cs
@@ -6,6 +6,8 @@ public class BoxController : MonoBehaviour
 {
 	[SerializeField]
 	private int CurrentWorld = 1;
+	[SerializeField]
+	private float KillingVelocity = 5;
 
 	private bool BeingGrabbed = false;
 
@@ -14,7 +16,7 @@ public class BoxController : MonoBehaviour
 		// Expected: if the box falls on the player, the player should die.
 		// The killing collider is smaller than the actual collider, so it should only touch the player if the box is falling fast.
 		// Just running into a still box shouldn't kill you. (Though you might hurt your toes)
-		if (other.tag == "Player" && (gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > 5))
+		if (other.tag == "Player" && (gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > KillingVelocity))
 		{
 			// Replace with actual respawn/room reset logic later
 			Destroy(other.gameObject);
