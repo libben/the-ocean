817002e2894b15a70223f766e017b2ccaf88b735
ho-eric <erho20@gmail.com>
2019-11-26 00:14:52 -0800

    Implement placeholder title screen, pregame scroll/picture, text prompt

---
+++ Ocean/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    private string SceneName;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneName = currentScene.name;
+        //Debug.Log(SceneName);
+        
+        if (SceneName == "PregameScroll")
+        {
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene("PrototypeRoom");
+            }
+        }  
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("PregameScroll");
+    }
+}
