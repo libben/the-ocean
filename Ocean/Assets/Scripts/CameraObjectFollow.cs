290969dd497101e08ca13ae69ed17fd55585b54a
wongband <bryanwong7788@gmail.com>
2019-12-03 03:12:19 -0800

    Added camera to character

--- Ocean/Assets/Scripts/CameraObjectFollow.cs
+++ Ocean/Assets/Scripts/CameraObjectFollow.cs
@@ -23,11 +23,12 @@ public class CameraObjectFollow : MonoBehaviour
         {
             this.Light.transform.position = new Vector3(this.Target.transform.position.x, this.Target.transform.position.y, 0.0f);
         }
-/*        if (this.Target)
+
+        if (this.Target)
         {
             var from = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
             var to = new Vector3(this.Target.transform.position.x, this.transform.position.y, this.transform.position.z);
-            transform.position = Vector3.Lerp(from, to, this.Speed);
-        }*/
+            transform.position = Vector3.Slerp(from, to, this.Speed);
+        }
     }
 }
