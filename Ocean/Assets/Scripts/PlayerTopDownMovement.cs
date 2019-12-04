0d8fedea8ff289ed112f5d0a4f40edc2a4185dd9
wongband <bryanwong7788@gmail.com>
2019-12-04 14:42:29 -0800

    Implemented light for the top down mechanic and fixed the top down camera

--- Ocean/Assets/Scripts/PlayerTopDownMovement.cs
+++ Ocean/Assets/Scripts/PlayerTopDownMovement.cs
@@ -12,18 +12,22 @@ namespace OceanGame
         private float RotationSpeed = 30.0f;
         [SerializeField]
         private Rigidbody2D PlayerBody;
+        [SerializeField]
+        private GameObject Light;
         private Vector2 Movement;
         private Vector2 MousePosition;
+        private Vector3 LightOffset = new Vector3(0.0f, .25f, 0.0f);
 
         void Update()
         {
             // This code is from the Unity Documentation from transform.up with
             // some slight altertaions https://docs.unity3d.com/ScriptReference/Transform-up.html
             
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKey(KeyCode.W))
             {
                 // Moves the gameobject in the direction you desire
-                transform.position += transform.up * this.MoveSpeed * Time.deltaTime;
+                this.transform.position += this.transform.up * this.MoveSpeed * Time.deltaTime;
+                this.Light.transform.position = this.transform.position + this.LightOffset;
             }
 
 /*            if (Input.GetKey(KeyCode.DownArrow))
@@ -32,13 +36,13 @@ namespace OceanGame
                 this.PlayerBody.velocity = -transform.forward * this.MoveSpeed;
             }*/
 
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.D))
             {
                 //Rotate the sprite about the Z axis in the negative direction
                 this.transform.Rotate(new Vector3(0, 0, -1 * this.RotationSpeed) * Time.deltaTime * this.MoveSpeed, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.A))
             {
                 //Rotate the sprite about the Z axis in the positive direction
                 this.transform.Rotate(new Vector3(0, 0, this.RotationSpeed) * Time.deltaTime * this.MoveSpeed, Space.World);
