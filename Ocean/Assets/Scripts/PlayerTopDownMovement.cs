a4a8479a137643b9973c12c588b5545d517bb271
wongband <bryanwong7788@gmail.com>
2019-12-03 23:21:06 -0800

    Implemented top down movement

--- Ocean/Assets/Scripts/PlayerTopDownMovement.cs
+++ Ocean/Assets/Scripts/PlayerTopDownMovement.cs
@@ -6,19 +6,43 @@ namespace OceanGame
 {
     public class PlayerTopDownMovement : MonoBehaviour
     {
+        [SerializeField]
         private float MoveSpeed = 5.0f;
+        [SerializeField]
+        private float RotationSpeed = 30.0f;
+        [SerializeField]
         private Rigidbody2D PlayerBody;
         private Vector2 Movement;
+        private Vector2 MousePosition;
 
         void Update()
         {
-            Movement.x = Input.GetAxisRaw("Horizontal");
-            Movement.y = Input.GetAxisRaw("Vertical");
-        }
+            // This code is from the Unity Documentation from transform.up with
+            // some slight altertaions https://docs.unity3d.com/ScriptReference/Transform-up.html
+            
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                // Moves the gameobject in the direction you desire
+                transform.position += transform.up * this.MoveSpeed * Time.deltaTime;
+            }
 
-        void FixedUpdate()
-        {
-            PlayerBody.MovePosition(this.PlayerBody.position + this.Movement * this.MoveSpeed * Time.fixedDeltaTime);
+/*            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
+                this.PlayerBody.velocity = -transform.forward * this.MoveSpeed;
+            }*/
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //Rotate the sprite about the Z axis in the negative direction
+                this.transform.Rotate(new Vector3(0, 0, -1 * this.RotationSpeed) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                //Rotate the sprite about the Z axis in the positive direction
+                this.transform.Rotate(new Vector3(0, 0, this.RotationSpeed) * Time.deltaTime * this.MoveSpeed, Space.World);
+            }
         }
     }
 }
