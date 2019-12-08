eb79dab8d7a1e729f799c3abaa625258c7425d9b
ho-eric <erho20@gmail.com>
2019-12-07 23:36:16 -0800

    Add improved dialogue box

---
+++ Ocean/Assets/Scripts/DialogueController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    [SerializeField] GameObject DialogueBox;
+    [SerializeField] private Text DialogueText;
+    private bool DialogueActive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DialogueActive && Input.anyKey)
+        {
+            DialogueBox.SetActive(false);
+            DialogueActive = false;
+        }
+    }
+
+    public void ShowDialogueBox(string Dialogue)
+    {
+        DialogueBox.SetActive(true);
+        DialogueActive = true;
+        DialogueText.text = Dialogue;
+
+    }
+}
