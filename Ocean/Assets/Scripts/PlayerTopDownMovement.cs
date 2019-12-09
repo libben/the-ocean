c824a4c6fb38acb7f7af825a9ba89f7e500e10f2
wongband <bryanwong7788@gmail.com>
2019-12-09 14:42:14 -0800

    Tweaked submarine movement

--- Ocean/Assets/Scripts/PlayerTopDownMovement.cs
+++ Ocean/Assets/Scripts/PlayerTopDownMovement.cs
@@ -7,45 +7,71 @@ namespace OceanGame
     public class PlayerTopDownMovement : MonoBehaviour
     {
         [SerializeField]
-        private float MoveSpeed = 5.0f;
+        private float CurrentThrust = 20.0f;
         [SerializeField]
-        private float RotationSpeed = 30.0f;
+        private float MaxThrust = 30.0f;
+        [SerializeField]
+        private float Accelertion = 1.25f;
+        [SerializeField]
+        private float CurrentAccelTime = 0.0f;
+        [SerializeField]
+        private float RotationSpeed = 3.0f;
+        [SerializeField]
+        private float SecondToStop = 0.0f;
         [SerializeField]
         private Rigidbody2D PlayerBody;
         [SerializeField]
         private GameObject Light;
-        private Vector2 Movement;
-        private Vector2 MousePosition;
         private Vector3 LightOffset = new Vector3(0.0f, .25f, 0.0f);
 
         void Update()
         {
             // This code is from the Unity Documentation from transform.up with
             // some slight altertaions https://docs.unity3d.com/ScriptReference/Transform-up.html
+            this.Light.transform.position = this.transform.position + this.LightOffset;
             
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
                 // Moves the gameobject in the direction you desire
-                this.transform.position += this.transform.up * this.MoveSpeed * Time.deltaTime;
-                this.Light.transform.position = this.transform.position + this.LightOffset;
+                UpdateMoveSpeed();
+                this.PlayerBody.AddForce(transform.up * this.CurrentThrust);
+            }
+            else
+            {
+                // Resets speed when stop moving altogether
+                this.CurrentThrust = 20.0f;
             }
 
-/*            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
-                this.PlayerBody.velocity = -transform.forward * this.MoveSpeed;
-            }*/
-
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
                 //Rotate the sprite about the Z axis in the negative direction
-                this.transform.Rotate(new Vector3(0, 0, -1 * this.RotationSpeed) * Time.deltaTime * this.MoveSpeed, Space.World);
+                this.transform.Rotate(new Vector3(0, 0, -1 * this.RotationSpeed) * Time.deltaTime * this.CurrentThrust, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
                 //Rotate the sprite about the Z axis in the positive direction
-                this.transform.Rotate(new Vector3(0, 0, this.RotationSpeed) * Time.deltaTime * this.MoveSpeed, Space.World);
+                this.transform.Rotate(new Vector3(0, 0, this.RotationSpeed) * Time.deltaTime * this.CurrentThrust, Space.World);
+            }
+        }
+
+        // Function to update the player's movement speed
+        void UpdateMoveSpeed()
+        {
+            this.CurrentAccelTime += Time.deltaTime;
+                
+            if (this.CurrentAccelTime > 1.0f) 
+            {
+                if (this.CurrentThrust > this.MaxThrust)
+                {
+                    this.CurrentThrust = this.MaxThrust;
+                }
+                else
+                {
+                    this.CurrentThrust += this.Accelertion;
+                }
+
+                this.CurrentAccelTime = 0.0f;
             }
         }
     }
