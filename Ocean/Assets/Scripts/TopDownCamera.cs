57b732bb629ee574c59448395b326d3312e2e14b
wongband <bryanwong7788@gmail.com>
2019-12-09 14:42:14 -0800

    Tweaked submarine movement

--- Ocean/Assets/Scripts/TopDownCamera.cs
+++ Ocean/Assets/Scripts/TopDownCamera.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 // Took the camera code from Hw1 and applied to move in all directions
 public class TopDownCamera : MonoBehaviour
 {
-    public GameObject Target;
-    public float Speed = 0.75f;
+    [SerializeField]
+    private GameObject Target;
+    [SerializeField]
+    private float Speed = 2.0f;
     private Vector3 TargetPosition;
+    private Vector3 CurrentPosition;
+    private Vector3 NextPosition;
 
     void Start()
     {
         this.TargetPosition = this.transform.position;
     }
 
+    void Update()
+    {
+        this.CurrentPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        this.NextPosition = new Vector3(this.Target.transform.position.x, this.Target.transform.position.y, this.transform.position.z);
+    }
+
     void LateUpdate()
     {
         if (this.Target)
         {
-            var from = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-            var to = new Vector3(this.Target.transform.position.x, this.Target.transform.position.y, this.transform.position.z);
-            transform.position = Vector3.Lerp(from, to, this.Speed);
+            transform.position = Vector3.Lerp(this.CurrentPosition, this.NextPosition, this.Speed);
         }
     }
 }
