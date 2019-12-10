d0dc5d0f83b589ce6218cbca428d797a0af08e7c
Ben Rausch <slimeguy46@gmail.com>
2019-12-10 00:17:31 -0800

    Add gravity gun pickup, Mars level

---
+++ Ocean/Assets/Scripts/LatchboxButtonController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheOcean {
+public class LatchboxButtonController : AbstractButtonController
+{
+    [SerializeField]
+    private GameObject LinkedObject;
+    private LatchboxController LinkedController;
+
+    // Start is called before the first frame update
+    void Start()
+    {	
+        if (!LinkedObject.TryGetComponent<LatchboxController>(out LinkedController))
+			Debug.Log("Error: Button couldn't find associated latchbox's controller.");
+    }
+
+        public override void OnTriggerEnter2D(Collider2D other)
+		{
+            base.OnTriggerEnter2D(other);
+			if (other.tag == "Player" || other.tag == "Box")
+			{
+				LinkedController.OpenLatchbox();
+			}
+		}
+
+		public override void OnTriggerStay2D(Collider2D other)		
+		{
+            base.OnTriggerStay2D(other);
+			if (other.tag == "Player" || other.tag == "Box")
+			{
+				LinkedController.OpenLatchbox();
+			}
+		}
+
+		public override void OnTriggerExit2D(Collider2D other)
+		{
+			base.OnTriggerExit2D(other);
+			LinkedController.CloseLatchbox();
+		}
+}
+}
