2a197ebe038e1a74da838b6d0a9ee1ca3ce37b4a
wongband <bryanwong7788@gmail.com>
2019-12-09 21:30:24 -0800

    Added burning sea, hatches and lights for each hatch

--- Ocean/Assets/Scripts/PlayerTopDownMovement.cs
+++ Ocean/Assets/Scripts/PlayerTopDownMovement.cs
@@ -7,15 +7,15 @@ namespace OceanGame
     public class PlayerTopDownMovement : MonoBehaviour
     {
         [SerializeField]
-        private float CurrentThrust = 20.0f;
+        private float CurrentThrust = 6.0f;
         [SerializeField]
-        private float MaxThrust = 30.0f;
+        private float MaxThrust = 10.0f;
         [SerializeField]
-        private float Accelertion = 1.25f;
+        private float Accelertion = 0.5f;
         [SerializeField]
         private float CurrentAccelTime = 0.0f;
         [SerializeField]
-        private float RotationSpeed = 3.0f;
+        private float RotationSpeed = 7.0f;
         [SerializeField]
         private float SecondToStop = 0.0f;
         [SerializeField]
@@ -29,7 +29,6 @@ namespace OceanGame
             // This code is from the Unity Documentation from transform.up with
             // some slight altertaions https://docs.unity3d.com/ScriptReference/Transform-up.html
             this.Light.transform.position = this.transform.position + this.LightOffset;
-            
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
                 // Moves the gameobject in the direction you desire
@@ -39,10 +38,17 @@ namespace OceanGame
             else
             {
                 // Resets speed when stop moving altogether
-                this.CurrentThrust = 20.0f;
+                this.CurrentThrust = 6.0f;
             }
 
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                //Rotate the sprite about the Z axis in the negative direction
+                this.transform.Rotate(new Vector3(0, 0, -1 * this.RotationSpeed) * Time.deltaTime * this.CurrentThrust, Space.World);
+            }
+
+            
+            if (Input.GetKey(KeyCode.E))
             {
                 //Rotate the sprite about the Z axis in the negative direction
                 this.transform.Rotate(new Vector3(0, 0, -1 * this.RotationSpeed) * Time.deltaTime * this.CurrentThrust, Space.World);
